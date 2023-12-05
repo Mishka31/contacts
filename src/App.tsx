7d1458bca91174624361338e23fc8cc56af87713
Misha <muzishutka7@gmail.com>
2023-12-05 19:18:06 +0200

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Container from './layouts/container/Container';
 import SocialLinks from './components/socialLinks/sociallinks';
+import Background from './components/background/background';
 
 function App() {
   return (
     <Container>
+      <Background />
       <SocialLinks />
     </Container>
   );
---
+++ src/components/background/background.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './background.module.css';
+
+const Background = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.background} />
+    </div>
+  );
+};
+
+export default Background;
