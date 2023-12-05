731981d7e11625e6aedda9d122d7e4e12d9f8fde
Misha <muzishutka7@gmail.com>
2023-12-05 14:30:07 +0200

    asd

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+if (document.documentElement.requestFullscreen) {
+  document.documentElement.requestFullscreen();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
