062c4aa1e4a659ae2ccc6eb48b11de878eef5f43
Misha <muzishutka7@gmail.com>
2023-12-07 03:32:30 +0200

    add player

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from './layouts/container/Container';
 import SocialLinks from './components/socialLinks/sociallinks';
 import Background from './components/background/background';
+import AvatarBlock from './components/avatarBlock/avatarBlock';
 import s from './App.module.css';
+import ReactPlayer from 'react-player';
 
 function App() {
+  const [onReady, setOnReady] = useState(false);
+
   return (
     <Container>
       <Background />
-      <div className={s.container}>
-        <img
-          className={s.img}
-          src="https://scontent-iev1-1.xx.fbcdn.net/v/t39.30808-6/321751407_1487309588420540_8675258260965107263_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=XVlRE0m_aSoAX96vmgO&_nc_ht=scontent-iev1-1.xx&oh=00_AfBWtam4BQqP9367QLjGA7_rznQXRQTNrrvRYAX67MzSKA&oe=65747F4A"
-          alt=""
-        />
-        <h1 className={s.nick}>@mike4soul</h1>
-
-        <h2 className={s.description}>
-          If you are not motivated by the result of your work, it is not yours ¯\_(ツ)_/¯
-        </h2>
-      </div>
+      <AvatarBlock />
       <SocialLinks />
+      <div className={s.container}>
+        {!onReady && <h1 className={s.h1}>Loading...</h1>}
+        <div className={s.back} />
+        <ReactPlayer
+          className={s.video}
+          controls={false}
+          url="https://www.youtube.com/watch?v=E1ce1zNMRJU&ab_channel=MishaKrasnonos"
+          playing={onReady}
+          onReady={() => setOnReady(true)}
+        />
+      </div>
     </Container>
   );
 }
